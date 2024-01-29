5686c6fd34959479b6365e3aed4cc64616113102
Echarawpascha <echarawpascha1@gmail.com>
2024-01-29 13:09:02 +0700

    belajar fungsii

--- fungsi.js
+++ fungsi.js
@@ -1,3 +1,4 @@
+
 var x = "25"; 
 var y = "3.14";
 var toNum =  parseInt(x);
@@ -8,6 +9,12 @@ console.log(toString + toString)
 function jumlah (a,b){
     return a + b;
 }
+var kurang = function (a,b){
+    return a - b;
+}
 alert(jumlah(5, 20))
+alert(kurang(25,5))
+
+
 
 
