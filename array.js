802d19ef5f2763e90d0979a4727366c2597a9bbb
Echarawpascha <echarawpascha1@gmail.com>
2024-01-29 13:35:17 +0700

    array

---
+++ array.js
@@ -0,0 +1,7 @@
+// var namaarray = new Array()                   
+var locker = new Array ("Wallet", 50000, true);
+console.log(locker[1])
+console.log(locker[2])
+
+locker[3] = "ktp";
+console.log(locker);
